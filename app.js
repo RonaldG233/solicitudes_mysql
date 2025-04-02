88b833d669794c6a3729107dade79b8e06655b1f
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2025-04-01 22:18:16 -0500

    tabla categoria hecho

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import categoriasRoutes from "./routes/categoriasRoutes.js";
+import productos_routes from "./routes/productos_routes.js"
 
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use(express.urlencoded({"extend":true}));
+app.use(express.urlencoded({"extended":true}));
 app.use('/categorias',categoriasRoutes);
+app.use("/productos",productos_routes);
 
 
 
